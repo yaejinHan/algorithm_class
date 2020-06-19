1edce81ed729d0b83b1546786ec7096d75745031
yaejinHan <65999222+yaejinHan@users.noreply.github.com>
2020-06-20 00:25:41 +0900

    Create efficient_adding.java

---
+++ hwk5/efficient_adding.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class efficient_adding {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        String content = "";
+        while ((line = br.readLine()) != null) {
+            content += line + " ";
+
+            if (!br.ready()) break;
+        }
+
+        br.close();
+
+        PriorityQueue<Long> q = new PriorityQueue<Long>();
+        String[] input = content.split(" ");
+        long sum = 0;
+        long cost = 0;
+        long indx = 0;
+
+        long n = Long.parseLong(input[0]);
+        long[] sumTable = new long[(int) n];
+
+        for (int i = 1; i <= n; i++) {
+        //    if (Integer.parseInt(input[i]) == 0) continue;
+            q.add(Long.parseLong(input[i]));
+        }
+
+        while (q.size() >=2) {
+            sum = q.poll() + q.poll();
+            q.add(sum);
+            sumTable[(int) indx] = sum;
+            indx++;
+        }
+
+
+        for (int i = 0; i < indx-1; i++) {
+            cost += sumTable[i];
+        }
+        cost += sum;
+        System.out.println(cost);
+	}
+
+}
